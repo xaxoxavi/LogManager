ba61ce52c70abe166e14bd14592ccdab70d6fe11
xaxoxavi <xavi.torrens@gmail.com>
2019-06-30 15:18:30 +0200

    UD3-EX4 SOLUCIÓ

--- src/main/java/log/LogFileManager.java
+++ src/main/java/log/LogFileManager.java
@@ -1,24 +1,63 @@
 package log;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  * Created by xavi on 17/10/17.
  */
 public class LogFileManager extends LogAbstractManager {
 
+    private String path;
+    private final String fileName = "/log.out";
+
+    public void init(){
+        File file = new File(path + fileName);
+        if (!file.exists()){
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 
     public void info(String text) {
+        if (isInfo()) appendFile(text);
 
     }
 
     public void error(String text) {
+        if (isError()) appendFile(text);
 
     }
 
     public void debug(String text) {
+        if (isDebug()) appendFile(text);
 
     }
 
     public void warning(String text) {
+        if (isWarning()) appendFile(text);
+
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    private void appendFile(String toAppend){
+        try {
+            BufferedWriter writer= new BufferedWriter(new FileWriter(path + fileName, true));
+            writer.append(toAppend);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
--- src/main/java/log/Main.java
+++ src/main/java/log/Main.java
@@ -12,7 +12,7 @@ public class Main
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext(
                 "SpringBeans.xml");
-        LogManager log = (LogManager) context.getBean("logger");
+        LogManager log = (LogManager) context.getBean("loggerFile");
         log.info("prova!!");
         
     }
